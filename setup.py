0960ac4e128e64fc21fa3e9113c2b248aeb1a9b1
StepProgrammer <itshark@mail.ru>
2015-04-09 17:33:59 +0300

    Add  Internet-Based Function

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PYTHON',
+    version='',
+    packages=['libs', 'Test'],
+    url='',
+    license='',
+    author='Владелец',
+    author_email='',
+    description=''
+)
