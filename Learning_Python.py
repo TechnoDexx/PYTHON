e1c06dfc1a7481842384f7379e21b88855e18e8a
StepProgrammer <itshark@mail.ru>
2015-09-16 20:55:47 +0300

    Add functions for data manipulation

--- Learning_Python.py
+++ Learning_Python.py
@@ -3,13 +3,33 @@ created on 01.02.2015
 
 @author: владелец
 '''
-import os
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-import libs
-from test import test
 
-hello.sayhello()
-test.test()
-print(os.environ['home'])
-print(internet.gethtml('http://mail.ru'))
-internet.html()
+max = 0
+print(a)
+def min(l):
+    min = 1000
+    for m in l:
+        if m < min:
+            min = m
+    return min
+
+def max(l):
+    max = 0
+    for g in l:
+        if g > max:
+            max = g
+    return max
+def  avg(l):
+
+    sum = 0
+    count = 0
+    for k in l:
+        sum = sum + k
+        count = count + 1
+    return sum / count
+
+print()
+print('min =', min(a),"| max = ", max(a))
+print(avg(a))
